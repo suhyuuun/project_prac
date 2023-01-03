ce8a8281036c68a38c6bf2aa891ccbef2b24bbcd
suhyun <suhyuuuuuun@gmail.com>
2023-01-04 04:21:56 +0900

    updte

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function team_vita500() {
+  location.href = "/team_vita500.html";
+}
+
+function curation_page() {
+  location.href = "/curation_page.html";
+}
+
+function one_km_map() {
+  location.href = "/one_km_map.html";
+}
+
+function magazine_page() {
+  location.href = "/magazine_page.html";
+}
+
+function my_page() {
+  location.href = "/my_page.html";
+}
+
+function main_page() {
+  location.href = "/index.html";
+}
+
+function login() {
+  location.href = "/login.html";
+}
+
+function sign_up() {
+  location.href = "/sign_up.html";
+}
+
+function review() {
+  location.href = "/review.html";
+}
